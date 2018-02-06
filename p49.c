8d633dcd3c14332e848fbfc103e304913bc46f2d
prakashmk <30828492+prakashmk@users.noreply.github.com>
2018-02-06 15:05:43 +0530

    Create p49.c

---
+++ p49.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+   int a,b;
+   printf("Enter the number:");
+   scanf("%d",&a);
+   printf("Enter the number:");
+   scanf("%d",&b);
+   printf("The sum of given number is:%d",a+b);
+}
