c364415db9987f66d2bbafcd6f564cd74664e9e8
prakashmk <30828492+prakashmk@users.noreply.github.com>
2018-02-08 09:43:07 +0000

    Create p41.c

---
+++ p41.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{    char a[10];
+     int count=0,b;
+    printf("Enter the string: ");
+    scanf("%s",a);
+    printf("enter the number:");
+    scanf("%d",&b);
+    for(count=0;count<=b;count++)
+    {
+    printf("%s\n",a);
+}
+}
